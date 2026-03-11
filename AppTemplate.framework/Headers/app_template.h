3ade2307de357d681ae9633a88e2d54c4721ef4d
Pooja Trivedi <pooja.trivedi@mapsted.com>
2026-03-11 22:41:08 +0530

    Update to SDK v6.2.15

---
+++ AppTemplate.framework/Headers/app_template.h
@@ -0,0 +1,19 @@
+//
+//  app_template_lamda.h
+//  app-template-lamda
+//
+//  Created by Parth Bhatt on 03/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for app_template_lamda.
+FOUNDATION_EXPORT double app_template_lamdaVersionNumber;
+
+//! Project version string for app_template_lamda.
+FOUNDATION_EXPORT const unsigned char app_template_lamdaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <app_template_lamda/PublicHeader.h>
+
+
+//#import <AppTemplateLambda/MDCBaseTextField.h>
